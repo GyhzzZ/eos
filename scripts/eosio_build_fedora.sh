f53bdd3e223ed79b81bb9d97abe5fe41014dcbbe
Nathan Pierce <nathan.pierce@block.one>
2018-12-20 09:59:18 -0500

    package name fix for fedora27

--- scripts/eosio_build_fedora.sh
+++ scripts/eosio_build_fedora.sh
@@ -67,7 +67,7 @@
 
 	printf "\\n"
 
-	DEP_ARRAY=( git sudo python3 procps which gcc.x86_64 autoconf automake libtool make cmake.x86_64 bzip2-devel.x86_64 wget bzip2 \
+	DEP_ARRAY=( git sudo python3 procps-ng which gcc.x86_64 autoconf automake libtool make cmake.x86_64 bzip2-devel.x86_64 wget bzip2 \
 				openssl-devel.x86_64 gmp-devel.x86_64 libstdc++-devel.x86_64 python2-devel.x86_64 python3-devel.x86_64 libedit.x86_64 ncurses-devel.x86_64 swig.x86_64 )
 	COUNT=1
 	DISPLAY=""

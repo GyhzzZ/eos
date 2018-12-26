8322826634f00eb5fdc95cd39b98fbda1f727b53
Nathan Pierce <nathan.pierce@block.one>
2018-12-26 14:09:00 -0500

    High Sierra

--- eosio_uninstall.sh
+++ eosio_uninstall.sh
@@ -21,6 +21,9 @@ if [ -d "${HOME}/opt/eosio" ]; then
             for binary in ${binaries[@]}; do
                rm ${binary}
             done
+            brew remove mongo-cxx-driver
+            brew remove mongo-c-driver
+            rm -rf ~/opt/* # Delete everything from opt
             # Handle cleanup of directories created from installation
             if [ "$1" == "--full" ]; then
                if [ -d $HOME/Library/Application\ Support/eosio ]; then rm -rf $HOME/Library/Application\ Support/eosio; fi # Mac OS

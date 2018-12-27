18cff3985a655a3e6d4ca6fefb1575c0637a7af2
Nathan Pierce <nathan.pierce@block.one>
2018-12-27 17:05:44 -0500

    artifact_path fixes

--- eosio_uninstall.sh
+++ eosio_uninstall.sh
@@ -24,7 +24,6 @@ if [ -d "${HOME}/opt/eosio" ]; then
             brew remove mongo-cxx-driver
             brew remove mongo-c-driver
             rm -rf ~/opt/* # Delete everything from opt
-            # Handle cleanup of directories created from installation
             if [ "$1" == "--full" ]; then
                if [ -d $HOME/Library/Application\ Support/eosio ]; then rm -rf $HOME/Library/Application\ Support/eosio; fi # Mac OS
                if [ -d $HOME/.local/share/eosio ]; then rm -rf $HOME/.local/share/eosio; fi # Linux
